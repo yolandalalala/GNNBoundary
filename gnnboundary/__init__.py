7e9f76cfb5be59f34f9c9c46feb8e9c654cb56aa
Xiaoqi Wang <xiaoqiwang2582@gmail.com>
2024-04-21 07:54:30 -0700

    update enzyme

--- gnnboundary/__init__.py
+++ gnnboundary/__init__.py
@@ -1,1 +1,5 @@
+from .criteria import *
+from .datasets import *
+from .models import *
+from .utils import *
 from gnn_xai_common import Evaluator, GraphSampler, Trainer
--- gnnboundary/utils/__init__.py
+++ gnnboundary/utils/__init__.py
@@ -1,3 +1,3 @@
+from gnn_xai_common.utils import *
 from .boundary_analysis import boundary_analysis
-from .draw_matrix import draw_matrix
 from .pairwise_boundary_analysis import pairwise_boundary_analysis
